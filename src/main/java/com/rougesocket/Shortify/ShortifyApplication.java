060c33be766ce4fd4c3e82a04006620617dcf6c4
Bhaskar Singh <rougesocket@gmail.com>
2026-01-08 09:12:48 +0530

    Initial commit for Shortify project

---
+++ src/main/java/com/rougesocket/Shortify/ShortifyApplication.java
@@ -0,0 +1,13 @@
+package com.rougesocket.Shortify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortifyApplication.class, args);
+	}
+
+}
